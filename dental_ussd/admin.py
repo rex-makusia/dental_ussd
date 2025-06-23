d2d0b385cb39f19b2a5bf30f55a6275e3d9f4a6b
rex-makusia <grecpt@gmail.com>
2025-06-23 19:56:00 +1000

    Define a admin for the dental ussd app

--- dental_ussd/admin.py
+++ dental_ussd/admin.py
@@ -1,1 +1,17 @@
 # Admin View for Dental USSD
+# Admin View for Dental USSD
+from django.contrib import admin
+from .models import Patient, Appointment, ClinicAvailability
+
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'mobile_number', 'created_at', 'updated_at')
+
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ('patient', 'appointment_type', 'clinic_location', 'appointment_date', 'status')
+
+
+@admin.register(ClinicAvailability)
+class ClinicAvailibilityAdmin(admin.ModelAdmin):
+    list_display = ('clinic_location', 'appointment_type', 'available_slots', 'appointment_date', 'updated_at')
